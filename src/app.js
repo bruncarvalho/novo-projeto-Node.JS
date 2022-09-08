f1419973577a710576448b126375b342571cd5cf
Bruna Carvalho <brunacarvalhocardoso@gmail.com>
2022-09-08 15:58:48 -0300

    atualização

--- src/app.js
+++ src/app.js
@@ -9,4 +9,4 @@ app.use(express.json())
 app.use(router)
 
 app.listen(3333, ()=>
-console.log('Servidor funcionando porta: 3333'))
+console.log('Servidor funcionando porta: http://localhost:3333/'))
